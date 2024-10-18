173a9cf87e1be74cd435ab1cf87b7d09badbc245
Atharv1610 <ad9694@srmist.edu.in>
2024-10-18 19:22:52 +0530

    Updated

--- base/views.py
+++ base/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id':1, 'name':'Lets earn python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend Developers'},
+]
+
 def home(request):
-    return render(request,'home.html')
+    return render(request,'home.html',{'rooms':rooms})
 
 def room(request):
     return render(request,'room.html')
