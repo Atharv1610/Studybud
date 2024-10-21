063b47a9ef14245bba16bf67f96fb409f57711ef
Atharv <atharv.aras18@gmail.com>
2024-10-22 00:15:56 +0530

    File updated

--- base/models.py
+++ base/models.py
@@ -15,7 +15,6 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete= models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     desciption = models.TextField(null=True, blank=True)
-
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
