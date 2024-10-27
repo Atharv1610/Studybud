6fd0a59b436f2c34a375c605b1614f88cdafdf5f
Atharv <atharv.aras18@gmail.com>
2024-10-28 00:25:52 +0530

    CRUD operation going to start

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('room/<str:pk>/',views.room, name='room') # name is beneficial when we are using url command in href tag
+    path('room/<str:pk>/',views.room, name='room'), # name is beneficial when we are using url command in href tag
+    path('create-room/', views.createRoom, name = "create-room" )
 ]
