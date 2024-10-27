08721a1634cdf6ae3626b0dcf4fbabfc71f7c41d
Atharv <atharv.aras18@gmail.com>
2024-10-27 23:01:01 +0530

    Add .gitignore to ignore env directory

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('room/<str:pk>/',views.room,name='room') # name is beneficial when we are using url command in href tag
+    path('room/<str:pk>/',views.room, name='room') # name is beneficial when we are using url command in href tag
 ]
